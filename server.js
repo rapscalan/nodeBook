f2860bb78f541ad66fb07916f38370ac611480a3
Zak Hyslin <rapscalan@gmail.com>
2019-12-09 00:21:15 -0800

    refactor

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const http = require('http');
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello world");
+        response.end();
+    }
 
-function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world");
-    response.end();
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+exports.start = start;
